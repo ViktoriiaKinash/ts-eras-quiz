4249ed7fcc78c89355a1152e58bc1e957de50242
Viktoriia Kinash <viktoriia.kinash@cern.ch>
2025-12-01 14:38:27 +0100

    Test bucket creation and deletion

--- cdk/stacks/infra_stack.py
+++ cdk/stacks/infra_stack.py
@@ -1,4 +1,4 @@
-from constructs import Construct
+from cdktf import Construct, TerraformStack, TerraformBackend
 from cdktf import TerraformStack
 from cdktf_cdktf_provider_google.provider import GoogleProvider
 from cdktf_cdktf_provider_google.storage_bucket import StorageBucket
@@ -6,6 +6,12 @@ from cdktf_cdktf_provider_google.storage_bucket import StorageBucket
 class InfraStack(TerraformStack):
     def __init__(self, scope: Construct, ns: str):
         super().__init__(scope, ns)
+
+        TerraformBackend.google(
+            self,
+            bucket="ts-eras-quiz-tfstate",
+            prefix="cdktf/infra-stack"
+        )
 
         GoogleProvider(
             self,
